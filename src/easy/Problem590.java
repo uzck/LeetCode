a9c85800409b5d38b265a590d88081236572583b
Uzck <uzchel@gmail.com>
2019-01-14 14:09:31 +0800

    problem590迭代版

--- src/easy/Problem590.java
+++ src/easy/Problem590.java
@@ -4,6 +4,7 @@ import basicstruct.Node;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Problem590 {
@@ -22,6 +23,29 @@ public class Problem590 {
         return result;
     }
 
+    public List<Integer> postOrderIteration(Node root) {
+        LinkedList<Node> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if (root == null) {
+            return output;
+        }
+
+        stack.add(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pollLast(); // 移除栈顶元素
+            output.addFirst(node.val); // 添加到链表头
+            if (node.children != null) {
+                for (Node item : node.children) {
+                    if (item != null) {
+                        stack.add(item);
+                    }
+                }
+            }
+
+        }
+        return output;
+    }
+
     public static void main(String[] args) {
         Node root = new Node();
         root.val = 1;
@@ -36,7 +60,7 @@ public class Problem590 {
         root.children.get(2).children.add(new Node(9, null));
         root.children.get(2).children.add(new Node(10, null));
         Problem590 problem590 = new Problem590();
-        List<Integer> result = problem590.postorder(root);
+        List<Integer> result = problem590.postOrderIteration(root);
         for (int i : result) {
             System.out.print(i + " ");
         }
